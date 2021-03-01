8a3de9ab1e0f6c10b4e7d8bc35b5bdf187cbf4e3
Joé Erny <joe.erny@gmail.com>
2021-03-01 15:42:02 +0100

    form

--- index.php
+++ index.php
@@ -2,44 +2,57 @@
 <html lang="fr">
 
 <head>
-    <title>TP1 - Joé Erny</title>
+    <title>TP1</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="style/style.css">
     <script src="script/script.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js%22%3E"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 </head>
 
+
+
 <body>
-    <h1>Formulaire :</h1>
-    <form class="needs-validation" method="post" onsubmit="verifyPasswords()">
-        <div>
-            Prénom : <input type="text" name="prenom" value="" />
-        </div>
-        <div>
-            Nom : <input type="text" name="nom" value="" />
-        </div>
-        <div>
-            <p>Civilité :</p>
-            <input type="radio" id="mr" name="civilite" value="mr">
-            <label for="mr">Monsieur</label>
-            <input type="radio" id="mme" name="civilite" value="mme">
-            <label for="mme">Madame</label>
-            <input type="radio" id="autre" name="civilite" value="autre" checked>
-            <label for="autre">Autre</label>
-        </div>
-        <div>
-            Mot de passe : <input id="pwd1" type="password" name="passe" value="" />
-        </div>
-        <div>
-            Valider le mot de passe : <input id="pwd2" type="password" name="passe" value="" />
-        </div>
-        <div>
-            Code postal : <input type="text" name="nom" value="" />
-        </div>
-        <div>
-            <input type="submit" value="Envoyer le formulaire">
-        </div>
-    </form>
+    <header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-body border-bottom shadow-sm">
+        <p class="h5 my-0 me-md-auto fw-normal">TP1 - Joé Erny</p>
+    </header>
+
+    <div class="container" style="text-align: center; margin: auto; margin-left: auto;margin-right: auto;  display: flex; justify-content: center;">
+        <h1>Formulaire :</h1>
+        <form class="needs-validation" method="post" onsubmit="verifyPasswords()">
+            <div class="row g-3">
+                <div class="col-sm-6">
+                    Prénom : <input type="text" name="prenom" value="" />
+                </div>
+            </div>
+            <div>
+                Nom : <input type="text" name="nom" value="" />
+            </div>
+            <div>
+                <p>Civilité :</p>
+                <input type="radio" id="mr" name="civilite" value="mr">
+                <label for="mr">Monsieur</label>
+                <input type="radio" id="mme" name="civilite" value="mme">
+                <label for="mme">Madame</label>
+                <input type="radio" id="autre" name="civilite" value="autre" checked>
+                <label for="autre">Autre</label>
+            </div>
+            <div>
+                Mot de passe : <input id="pwd1" type="password" name="passe" value="" />
+            </div>
+            <div>
+                Valider le mot de passe : <input id="pwd2" type="password" name="passe" value="" />
+            </div>
+            <div>
+                Code postal : <input type="text" name="nom" value="" />
+            </div>
+            <div>
+                <input type="submit" value="Envoyer le formulaire">
+            </div>
+        </form>
+
+
+    </div>
 </body>
 
 </html>
